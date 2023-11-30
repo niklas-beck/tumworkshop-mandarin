7940b70f7ea4cfd6622f7ae17e2dd1ff67f3a8ca
kbcoder19 <117324783+kbcoder19@users.noreply.github.com>
2023-11-30 18:50:15 +0100

    Update function_app.py
    implemented sum fucntion

--- src/function_app.py
+++ src/function_app.py
@@ -27,8 +27,23 @@ def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
 
 
 # Add a new function here
-def sum()->int:
-    a=int(input("provide a")
-    b=int(input("provide b")
-    return a+b
+def sum(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get(a,b)
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        return func.HttpResponse(f"sum of {a} + {b} ={a+b}")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. But who are you?",
+             status_code=200
+        )
 # TODO
